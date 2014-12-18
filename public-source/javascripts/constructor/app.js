f37983d26856b236f212469da5ce92bceda61a71
dennisleenders <dennisleendersmv@gmail.com>
2014-12-18 15:42:15 +0100

    trying out new library

--- public-source/javascripts/constructor/app.js
+++ public-source/javascripts/constructor/app.js
@@ -1,11 +1,34 @@
+///// oauth
+var oauth = OAuth({
+    consumer: {
+        public: 'f5fa91bedfd54f5da77584ce4c11dbbf',
+        secret: '84228963d5e84c1a87027cad68e16081'
+    },
+    signature_method: 'HMAC-SHA1'
+});
 
-var app = /**
-* nounTranslate Module
-*
-* Description
-*/
-angular.module('nounTranslate', []);
+////// angular
+var app = angular.module('nounTranslate', []);
 
-app.controller('iconController');
+app.controller('apiController', function(){
+  console.log("check");
 
-app.factory('nounProject');
+  var request_data = {
+      url: 'http://api.thenounproject.com/icon/1',
+      method: 'GET'
+  };
+
+  var token = {
+    public: 'f5fa91bedfd54f5da77584ce4c11dbbf',
+    secret: '84228963d5e84c1a87027cad68e16081'
+  };
+
+  $.ajax({
+      url: request_data.url,
+      type: request_data.method,
+      data: oauth.authorize(request_data, token)
+  }).done(function(data) {
+      //process your data here
+  });
+
+});
