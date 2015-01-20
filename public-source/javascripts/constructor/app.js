7904bf38b2470fa8087dce7ac233bbb6ed2b318d
dennisleenders <dennisleendersmv@gmail.com>
2015-01-20 09:34:45 +0100

    added hide/show of the empty container

--- public-source/javascripts/constructor/app.js
+++ public-source/javascripts/constructor/app.js
@@ -4,9 +4,10 @@ var app = angular.module('nounTranslate', []);
 app.controller('apiController', function($scope, $http){
 
   var searchField = $('.search-field');
+  var cardPosition = $('.card-position');
   var inputValue;
-  //var json;
-
+  
+  // On the keydown enter we will do a callback to the API of NOUNproject
   searchField.on('keydown',function(e){
     if(e.keyCode == 13){
       inputValue = this.value;
@@ -20,14 +21,17 @@ app.controller('apiController', function($scope, $http){
           } 
       };
 
+      // We use $HTTP since it will trigger a "digest", digests will update things like ng-repeat
+      // or live code injection
       $http({
         url: request_data.url,
         method: request_data.method,
         params: {term:request_data.data.term,site:request_data.data.site}
       }).success(function(data){
-        //json = data;
+        // If the call was a success we will set the json to the Controller's $scope (this).
+        // This way our directives in the html can read them.
         $scope.iconResults = data.icons;
-        console.log($scope.iconResults[0]);
+        cardPosition.show();
       }).error(function(data){
         console.log("Ajax fail");
       });
