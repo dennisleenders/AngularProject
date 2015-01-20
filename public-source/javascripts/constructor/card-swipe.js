176da0a328f094afef2b4fa7de71b67a08686fa1
dennisleenders <dennisleendersmv@gmail.com>
2015-01-20 11:21:45 +0100

    added styles for swiping and card correction styles

--- public-source/javascripts/constructor/card-swipe.js
+++ public-source/javascripts/constructor/card-swipe.js
@@ -2,18 +2,45 @@
 var cardSwipeEl = document.getElementById('card-swipe');
 var cardEl = new Hammer(cardSwipeEl);
 var ticker = 0;
+var isSwiped = false;
 
 cardEl.on("panright", function(e) {
-    console.log("right");
-    swapCard(ticker)
+    swapCard(ticker, "right")
 });
 
 cardEl.on("panleft", function(e) {
-    console.log("left");
-    swapCard(ticker);
+    swapCard(ticker, "left");
 });
 
-function swapCard(ticker) {
-  var foundtarget = $("#card-swipe").find("icon-card:nth-child(0)");
-  console.log(foundtarget);
+
+// funtion that swaps the cards around. 
+// It first checks if the card swap animation is still in progress &&
+// if there are even cards in the ng-repeat (cards.length will be updated)
+// since ng-repeat will dynamicly create more cards when we search.
+function swapCard(position, direction) {
+  var cards = $(".icon-card"); 
+  if(isSwiped == false && cards.length != 0){
+    var selectedCard;
+    var nthChild;
+    console.log("check")
+    if(direction == "left"){
+      nthChild = ":nth-child(" + (cards.length - position) + ")";
+      selectedCard = $(".icon-card" + nthChild)
+      selectedCard.addClass("swiped");
+      ticker++
+    }else if(ticker >= 1){
+      nthChild = ":nth-child(" + (cards.length - position + 1) + ")";
+      selectedCard = $(".icon-card" + nthChild);
+      selectedCard.removeClass("swiped");
+      ticker--
+    };
+    swapCardTimeout();
+  }
+}
+
+function swapCardTimeout() {
+  isSwiped = true;
+  setTimeout(function() {
+    isSwiped = false;
+  }, 400);
 }
